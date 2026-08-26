c1954f6eaf2179b2840d9ff08d32f33495f8d852
Engr-Jawad <jawadbuniri2@gmail.com>
2026-08-26 12:03:11 +0500

    level one problem are solved

---
+++ Day_one.py
@@ -0,0 +1,75 @@
+# Write a program that asks the user for their name and age.
+# ----------------------------------
+# name = str(input("enter your name : "))
+# age = int(input("Enter your age : "))
+# print(f"Hello i am {name} and i am {age} years old .")
+
+
+
+
+# Q2. Take two numbers as input from the user and print their sum, difference,product, and quotient.
+# ---------------------------------------------
+# num_one = int(input("enter the ist number : "))
+# num_two = int(input("enter the 2nd number : "))
+# add = num_one + num_two
+# sub = num_one - num_two
+# mul = num_one * num_two
+# quoteint = num_one / num_two
+# print("Add = ",add)
+# print("Sub = ",sub)
+# print("Mul = ",mul)
+# print("Quoteint = ",quoteint)
+
+
+
+
+#Ask the user to enter two integers and one float. Convert them all to floats and print their average.
+# -------------------------------------------------
+# num_one = int(input("enter the ist number : "))
+# num_two = int(input("enter the 2nd number : "))
+# average = float((num_one + num_two )/ 2 )
+# print(average)
+
+
+
+# The user enters a string containing a number (e.g., "45" ). Convert it to:
+# • an integer
+# • a float
+# • a string again
+# Print all three values with their types
+# -----------------------------------------
+# strr = str(input("enter the string :  "))
+# number = int(strr)
+# print(type(number),number )
+# number = float(strr)
+# print(type(number),number )
+
+# number = str(strr)
+# print(type(number),number)
+
+
+
+# Evaluate and print the result of the following expression:
+# --------------------------------------
+# x = 10 + 3 * 2 ** 2
+# print(x)
+
+
+
+#----------------------------------------
+# Write a program to swap values of two numbers entered by the user.
+# numone = int(input("enter the ist number : "))
+# numtwo = int(input("enter the 2nd number : "))
+# numone,numtwo = numtwo,numone
+# print("ist number : ",numone)
+# print("2nd number : ",numtwo)
+
+
+
+
+# Q7. Ask the user for a temperature in Celsius (string input). Convert it to float ,
+# then calculate and print temperature in Fahrenheit.
+# Conversion formula: F ahrenheitT emp = (CelsiusT emp ∗ (9/5)) +32)
+# celsius = float(input("enter the temprature : "))
+# Fahrenheite  = (celsius * (9/5) )+ 32
+# print("Fahrenheite : ",Fahrenheite)
