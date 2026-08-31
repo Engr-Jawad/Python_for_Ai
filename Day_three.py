ae8bc4d4a116c1bb27cae0c9d9c21f376cea0c63
Engr-Jawad <jawadbuniri2@gmail.com>
2026-08-31 20:33:47 +0500

    Day three ist problem are solved

---
+++ Day_three.py
@@ -0,0 +1,23 @@
+# problem one 
+# Ask the user for a string and check whether it is a palindrome or not.
+# A palindrome is a string which is same when we read it forward & backward. Eg -
+# “madam”, “racecar” etc.
+
+
+""" def palindrom(string):
+    rev ="" 
+    for i in string:
+        rev = i + rev 
+    if rev == string:
+        print("the string is palindrome")
+    else:
+        print("the string is not palindrome")
+    
+if __name__ == "__main__":
+    string = str(input("enter the string : "))
+    palindrom(string) 
+    """
+    
+
+
+
